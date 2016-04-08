b5c09b1bad1a5107e51a04b3c67c584cc3e5a6f4
Ramiro Araujo <rama.araujo@gmail.com>
2016-04-08 18:31:43 -0300

    cakephp console completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,41 @@ eval "$(gulp --completion=zsh)"
 eval "$(grunt --completion=zsh)"
 
 #. <(npm completion)
+
+_cakephp () {
+    if [ ! -f bin/cake ] ; then
+        return
+    fi
+
+    local commands command subcommands command_options subcommand_options
+
+    commands=$(bin/cake completion commands)
+
+    if [[ "$CURRENT" = 2 ]]; then
+        compadd `echo ${commands}`
+        return 0
+    fi
+
+    command=$words[2]
+    subcommands=`bin/cake completion subcommands ${command}`
+    command_options=`bin/cake completion options ${command}`
+
+    if [[ "$CURRENT" = 3 ]] ; then
+        compadd `echo ${subcommands}`
+        return 0
+    fi
+
+        if [[ "$CURRENT" = 4 ]] ; then
+        compadd `echo ${command_options}`
+        return 0
+    fi
+}
+
+compdef _cakephp bin/cake
+compdef _cakephp cake
+
+#Alias
+#alias c3='bin/cake'
+
+#alias c3cache='bin/cake orm_cache clear'
+#alias c3migrate='bin/cake migrations migrate'
